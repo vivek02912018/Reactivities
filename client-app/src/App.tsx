89732418f80fcc6ccb0b244aefecd03a3b0ada13
vivek <visviv@gmail.com>
2020-04-05 20:26:10 +0530

    Test

--- client-app/src/App.tsx
+++ client-app/src/App.tsx
@@ -1,24 +1,10 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import {cars}   from './demo';
-//import {posts} from './demo';
-import CarItem from './CarItem';
+
 
  
 
-function App(props: { cars: any[]; }) {
-return (
-  <div className="App">
-    <ul>
-      {                   
-        props.cars.map((car)=>(
-          <CarItem car ={car}/>))
-      }
-    </ul>
-  </div>
-);
-}
   
 
 export default App;
